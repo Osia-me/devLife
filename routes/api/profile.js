f896dd2b8957b4967098e02ddaa54f5492c03947
Osia-me <olga.melnykovych@gmail.com>
2019-04-02 14:35:32 +0200

    profile schema is created, route for profile logic included

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,9 @@
-const express = require('express'),
-      router  = express.Router();
+const express   = require('express'),
+      router    = express.Router()
+      mongoose  = require('mongoose'),
+      passport  = require('passport'),
+      Profile   = require('../../models/Profile'),
+      User      = require('../../models/User');
 
 // @route GET api/profile/test
 // @description To test profile route
@@ -7,5 +11,22 @@ const express = require('express'),
 router.get('/test', (req, res) => res.json({
         msg: 'Profile works'
       }));
+
+// @route GET api/profile
+// @description To get current users profile
+// @access Privat
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.user.id })
+    .then(profile => {
+      if(!profile){
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json("Status not found!"));
+});
 
 module.exports = router;
