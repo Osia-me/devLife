d9cfc6a5cd6da3f655b29265ae36d3fb74726bfa
Osia-me <olga.melnykovych@gmail.com>
2019-04-12 13:13:19 +0200

    created model for topics and get topics-all existing, post potic and delete by id routes

---
+++ models/Topic.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const TopicSchema = new mongoose.Schema({
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    text: {
+      type: String,
+      require: true
+    },
+    name: {
+      type: String
+    },
+    avatar: {
+      type: String
+    },
+    likes: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        }
+      }
+    ],
+    comments: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User'
+        },
+        text: {
+          type: String,
+          required: true
+        },
+        avatar: {
+          type: String
+        },
+        date: {
+          type: Date,
+          default: Date.now
+        }
+      }
+    ],
+    date: {
+      type: Date,
+      default: Date.now
+    }
+});
+
+module.exports =  mongoose.model('Topics', TopicSchema);
--- routes/api/topics.js
+++ routes/api/topics.js
@@ -1,5 +1,13 @@
-const express = require('express'),
-      router  = express.Router();
+const express       = require('express'),
+      router        = express.Router(),
+      mongoose      = require('mongoose'),
+      passport      = require('passport'),
+      //Model
+      Profile       = require('../../models/Profile'),
+      Topic         = require('../../models/Topic'),
+      //Validation
+      validateTopic = require('../../validation/topics');
+
 
 // @route GET api/topics/test
 // @description To test topics route
@@ -7,5 +15,68 @@ const express = require('express'),
 router.get('/test', (req, res) => res.json({
         msg: 'Topics works'
       }));
+
+// @route GET api/topics
+// @description Get all topics
+// @access Public
+router.get('/', (req,res) => {
+  Topic.find()
+  //Show topics by the newest
+    .sort({date: -1})
+    //Return all topics
+    .then(topics => res.json(topics))
+    .catch(err => res.status(404).json('There is no topics to show'));
+});
+
+// @route GET api/topics/:id
+// @description Get topic by id
+// @access Public
+router.get('/:id', (req, res) => {
+  Topic.findById(req.params.id)
+    .then(topic => res.json(topic))
+    .catch(err => res.status(404).json('There is no topic with this id'));
+});
+
+// @route POST api/topics
+// @description Create topic
+// @access Privat
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  const {errors, isValid} = validateTopic(req.body);
+
+  if(!isValid){
+    ///Send all errors
+    return res.status(400).json(errors);
+  }
+
+  const newPost = new Topic({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+
+  newPost.save().then(post => res.json(post)).catch(err => res.json(err));
+});
+
+// @route DELETE api/topics/:id
+// @description Delete topic
+// @access Privat
+
+router.delete('/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      Topic.findById(req.params.id)
+      .then(topic => {
+        //Check if you own this topic
+        if(topic.user.toString() !== req.user.id){
+          return res.status(401).json({notauthorized: 'User not authorized'});
+        }
+        //Delete
+        topic.remove().then(() => res.json({success: true}))
+      })
+      .catch(err => res.json('Post not found'))
+    })
+});
+
 
 module.exports = router;
