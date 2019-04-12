de906477013f07bf47d2987ff88a376ee2d16c90
Osia-me <olga.melnykovych@gmail.com>
2019-04-12 12:04:33 +0200

    delete experience by id, delete education by id and delete profile together with user routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -219,7 +219,7 @@ router.post('/education', passport.authenticate('jwt', {session: false}), (req,r
 // @route Delete api/profile/experience/:id
 // @description Delete experience from profile
 // @access Privat
-router.post('/experience/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.delete('/experience/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
   Profile.findOne({user: req.user.id})
     .then(profile => {
       //Get remove index
@@ -227,10 +227,40 @@ router.post('/experience/:id', passport.authenticate('jwt', {session: false}), (
       .indexOf(req.params.id);
       //Splice out of array
       profile.experience.splice(removeIndex, 1);
-      profile.save().then(profile = > res.json(profile));
+      profile.save().then(profile => res.json(profile));
     })
     .catch(err => res.status(404).json(err));
 });
+
+// @route Delete api/profile/education/:id
+// @description Delete experience from profile
+// @access Privat
+router.delete('/education/:id', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOne({user: req.user.id})
+    .then(profile => {
+      //Get remove index
+      const removeIndex = profile.education.map(item => item.id)
+      .indexOf(req.params.id);
+      //Splice out of array
+      profile.education.splice(removeIndex, 1);
+      profile.save().then(profile => res.json(profile));
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route Delete api/profile
+// @description Delete profile and user
+// @access Privat
+router.delete('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Profile.findOneAndRemove({user: req.user.id})
+    .then(() => {
+      User.findOneAndRemove({_id: req.user.id})
+        .then(() => res.json({success: true}))
+        .catch(err => res.json(err))
+    })
+    .catch(err => res.json(err))
+});
+
 
 
 module.exports = router;
